a0bf6b15ffbd3a7e016046bfaa47d213043dd48b
sts219 <sts219@ic.ac.uk>
2022-02-02 12:31:05 +0000

    Starting to rewrite sensor stuff

--- ccs811.py
+++ ccs811.py
@@ -1,15 +1,55 @@
+from asyncio.windows_events import NULL
 import smbus2
 from time import sleep
 
-SLAVE_ADDR = 0x5A # Can be 0x5A
+# Slave register addresses
+class SENSOR_REGISTERS():
+    STATUS = 0x00 # R
+    MEAS_MODE = 0x01 # R/W
 
-# Slave registers to read and write
-DEVICE_REG_STATUS = 0x00
-DEVICE_REG_MEAS_MODE = 0x01
-DEVICE_REG_ALG_RESULT_DATA = 0x02
-DEVICE_REG_ERROR_ID = 0xE0
-DEVICE_REG_APP_START = 0xF4
+    # The most signifcant 2 bytes contain a ppm estimate of eCO2 level, next 2 bytes contain a ppg estimate of the total VOC level
+    ALG_RESULT_DATA = 0x02 # R
 
+    # Temperature / humidity data can be written to enable compensation
+    ENV_DATA = 0x05 # W
+
+    # Error source location
+    ERROR_ID = 0xE0 # R
+
+class SENSOR():
+    ADDR = 0x5A # Can be 0x5B, check I2C connection
+    REGS = SENSOR_REGISTERS()
+
+    def __init__(self, bus):
+        self.bus = bus
+        self.status = self.bus.read_byte_data(self.ADDR, self.REGS.STATUS)
+
+    def updateStatus(self, bus):
+        self.status = bus.read_byte_data(self.ADDR, self.REGS.STATUS)
+
+    def print_status(self):
+        status = self.status
+        FW_MODE = getBit(status, 7)
+        print(f'FW_MODE: {FW_MODE}')
+        assert FW_MODE == 1, "ERROR: Sensor is in boot mode"
+        APP_VALID = getBit(status, 4)
+        print(f'APP_VALID: {APP_VALID}')
+        assert APP_VALID == 1, "ERROR: Sensor does not have valid application firmware"
+        DATA_READY = getBit(status, 3)
+        print(f'DATA_READY: {DATA_READY}')
+        ERROR = getBit(status, 0)
+        print(f'ERROR: {ERROR}')
+        if DATA_READY == 1: print('Data is ready to be read')
+        if ERROR == 1: print('Read ERROR_ID')
+
+
+def getBit(val, idx):
+    return (val & (1 << idx)) >> idx
+
+def new_data(status):
+    getBit(status, 3)
+
+""" Deal with these values later
 # Slave register read values
 DEVICE_STATE_BOOT = 0x10
 DEVICE_STATE_APP = 0x90
@@ -18,47 +58,13 @@ DEVICE_STATE_APP_WITH_DATA = 0x98
 # Slave register write values
 DEVICE_SET_MODE_10S = [0x10]
 DEVICE_SET_SW_RESET = [0x11, 0xE5, 0x72, 0x8A]
+"""
 
-# 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-i2c_bus = smbus2.SMBus(1)
+# Run this function
+def startup(bus, device):
+    status = bus.read_byte_data(device.ADDR, device.REGS.STATUS)
+    device.print_status(status)
 
-def read_i2c_bus_dev(dev_reg, read_len):
-    return i2c_bus.read_i2c_block_data(SLAVE_ADDR, dev_reg, read_len)
-
-def write_i2c_bus_dev(dev_reg, write_data):
-    i2c_bus.write_i2c_block_data(SLAVE_ADDR, dev_reg, write_data)
-
-try:
-    print("Hello from python")
-    if read_i2c_bus_dev(DEVICE_REG_STATUS, 1)[0] == DEVICE_STATE_BOOT:
-        write_i2c_bus_dev(DEVICE_REG_APP_START, []) # empty write
-        write_i2c_bus_dev(DEVICE_REG_MEAS_MODE, DEVICE_SET_MODE_10S)
-
-    read_i2c_bus_dev(DEVICE_REG_ERROR_ID, 1) # clear any errors
-
-    timeout_in_seconds = 30
-
-    while (timeout_in_seconds and
-           (read_i2c_bus_dev(DEVICE_REG_STATUS, 1)[0] !=
-            DEVICE_STATE_APP_WITH_DATA)):
-              sleep(5)
-              timeout_in_seconds -= 5
-
-    if not timeout_in_seconds:
-        raise
-
-    read_data = read_i2c_bus_dev(DEVICE_REG_ALG_RESULT_DATA, 8)
-
-    eCO2_reading = (read_data[0] << 8) | read_data[1]
-    eTVOC_reading = (read_data[2] << 8) | read_data[3]
-
-    # Return some Dialtone!
-    print ('{{"chans":['
-              '{{"ch_idx":0,"ch_data":[{{"data_idx":0,"units":1,"val":{0}}}]}},'
-              '{{"ch_idx":1,"ch_data":[{{"data_idx":0,"units":1,"val":{1}}}]}}'
-           ']}}').format(eCO2_reading, eTVOC_reading)
-
-except Exception as e:
-    pass
-finally:
-    i2c_bus.close()
+# I2C Bus
+bus = smbus2.SMBus(1)
+sensor = SENSOR(bus)
